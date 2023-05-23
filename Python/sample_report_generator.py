36f658b3d1b5b876363840331e659ea141e66ea4
John McDonough <movinalot@gmail.com>
2023-05-22 23:18:53 -0700

    Update sample_report_generator.py
    pylint and black

--- Python/sample_report_generator.py
+++ Python/sample_report_generator.py
@@ -1,16 +1,20 @@
+"""
+    Sample report generator
 
-#
-# Sample report generator
-#
-# Based on FlexVM API, created by John McDonough (@movinalot) - Fortinet
+    Based on FlexVM API, created by Daniel Romio (@Forti-Cloud-BDE) - Fortinet
 
-import pandas as pd
-import requests
+    Checked by FortiDevSec
+"""
+
+# pylint: disable=too-many-arguments
+
+from datetime import datetime, timedelta
+import logging
 import json
 import os
 import sys
-import logging
-from datetime import datetime, timedelta
+import pandas as pd
+import requests
 
 
 logger = logging.getLogger()
@@ -30,7 +34,9 @@ def requests_post(resource_url, json_body, headers):
     logging.debug(headers)
 
     try:
-        result = requests.post(resource_url, json=json_body, headers=headers)
+        result = requests.post(
+            resource_url, json=json_body, headers=headers, timeout=20
+        )
     except requests.exceptions.RequestException as error:
         raise SystemExit(error) from error
     if result.ok:
@@ -67,7 +73,7 @@ def programs_list(access_token):
 
     uri = FLEXVM_API_BASE_URI + "programs/list"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     results = requests_post(uri, "", headers)
     return results
@@ -81,7 +87,7 @@ def configs_create(
 
     uri = FLEXVM_API_BASE_URI + "configs/create"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {
         "programSerialNumber": program_serial_number,
@@ -100,7 +106,7 @@ def configs_disable(access_token, config_id):
 
     uri = FLEXVM_API_BASE_URI + "configs/disable"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"id": config_id}
 
@@ -114,7 +120,7 @@ def configs_enable(access_token, config_id):
 
     uri = FLEXVM_API_BASE_URI + "configs/enable"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"id": config_id}
 
@@ -128,7 +134,7 @@ def configs_list(access_token, program_serial_number):
 
     uri = FLEXVM_API_BASE_URI + "configs/list"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"programSerialNumber": program_serial_number}
 
@@ -142,7 +148,7 @@ def configs_update(access_token, config_id, name, cpu, svc_package):
 
     uri = FLEXVM_API_BASE_URI + "configs/update"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {
         "id": config_id,
@@ -160,7 +166,7 @@ def groups_list(access_token):
 
     uri = FLEXVM_API_BASE_URI + "groups/list"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     results = requests_post(uri, "", headers)
     return results
@@ -172,7 +178,7 @@ def groups_nexttoken(access_token, folder_path):
 
     uri = FLEXVM_API_BASE_URI + "groups/nexttoken"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"folderPath": folder_path}
 
@@ -186,7 +192,7 @@ def vms_create(access_token, config_id, count, description, end_date):
 
     uri = FLEXVM_API_BASE_URI + "vms/create"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {
         "configId": config_id,
@@ -205,7 +211,7 @@ def vms_list(access_token, config_id):
 
     uri = FLEXVM_API_BASE_URI + "vms/list"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"configId": config_id}
 
@@ -219,7 +225,7 @@ def vms_points_by_config_id(access_token, config_id, start_date, end_date):
 
     uri = FLEXVM_API_BASE_URI + "vms/points"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"configId": config_id, "startDate": start_date, "endDate": end_date}
 
@@ -233,7 +239,7 @@ def vms_points_by_serial_number(access_token, vm_serial_number, start_date, end_
 
     uri = FLEXVM_API_BASE_URI + "vms/points"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {
         "serialNumber": vm_serial_number,
@@ -272,7 +278,7 @@ def vms_update(access_token, vm_serial_number, config_id, description, end_date)
     if process_update:
         uri = FLEXVM_API_BASE_URI + "vms/update"
         headers = COMMON_HEADERS.copy()
-        headers["Authorization"] = "Bearer {0}".format(access_token)
+        headers["Authorization"] = f"Bearer {access_token}"
 
         body = {
             "serialNumber": vm_serial_number,
@@ -297,7 +303,7 @@ def vms_reactivate(access_token, vm_serial_number):
 
     uri = FLEXVM_API_BASE_URI + "vms/reactivate"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"serialNumber": vm_serial_number}
 
@@ -311,7 +317,7 @@ def vms_stop(access_token, vm_serial_number):
 
     uri = FLEXVM_API_BASE_URI + "vms/stop"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"serialNumber": vm_serial_number}
 
@@ -325,12 +331,13 @@ def vms_token(access_token, vm_serial_number):
 
     uri = FLEXVM_API_BASE_URI + "vms/token"
     headers = COMMON_HEADERS.copy()
-    headers["Authorization"] = "Bearer {0}".format(access_token)
+    headers["Authorization"] = f"Bearer {access_token}"
 
     body = {"serialNumber": vm_serial_number}
 
     results = requests_post(uri, body, headers)
     return results
+
 
 def get_first_date_of_current_month(year, month):
     """Return the first date of the month.
@@ -348,7 +355,7 @@ def get_first_date_of_current_month(year, month):
 
 def get_last_date_of_month(year, month):
     """Return the last date of the month.
-    
+
     Args:
         year (int): Year, i.e. 2022
         month (int): Month, i.e. 1 for January
@@ -356,27 +363,28 @@ def get_last_date_of_month(year, month):
     Returns:
         date (datetime): Last date of the current month
     """
-    
+
     if month == 12:
         last_date = datetime(year, month, 31)
     else:
         last_date = datetime(year, month + 1, 1) + timedelta(days=-1)
-    
+
     return last_date.strftime("%Y-%m-%d")
 
 
 # Recommended to set the variables in your desktop emvironment
 
-API_USERNAME = os.getenv("API_USERNAME", "api-username-goes-here")
-API_PASSWORD = os.getenv("API_PASSWORD", "api-password-goes-here")
+FLEXVM_ACCESS_USERNAME = os.getenv("FLEXVM_ACCESS_USERNAME", "api-username-goes-here")
+FLEXVM_ACCESS_PASSWORD = os.getenv("FLEXVM_ACCESS_PASSWORD", "api-password-goes-here")
 API_CLIENT_ID = os.getenv("API_CLIENT_ID", "flexvm")
 API_GRANT_TYPE = os.getenv("API_GRANT_TYPE", "password")
 
 
 # Get API Token
-api_token = get_token(API_USERNAME, API_PASSWORD, API_CLIENT_ID, API_GRANT_TYPE)
+api_token = get_token(FLEXVM_ACCESS_USERNAME, FLEXVM_ACCESS_PASSWORD, API_CLIENT_ID, API_GRANT_TYPE)
 if api_token:
-    LOG_MESSAGE = "API access_token: {0}".format(api_token["access_token"])
+    api_access_token = api_token["access_token"]
+    LOG_MESSAGE = f"API access_token: {api_access_token}"
     logging.debug(LOG_MESSAGE)
     api_access_token = api_token["access_token"]
 else:
@@ -388,54 +396,61 @@ if programs_list:
     print(programs_list)
 
 # Create a dataframe to store the programs
-programs = pd.DataFrame(programs_list['programs'])
+programs = pd.DataFrame(programs_list["programs"])
 
 # Please, replace this variable with the relevant program SN for your environment
-# Example : MY_PROGRAM = 'ELAVMS0000000467' 
-MY_PROGRAM = 'ELAVMS0000000467' 
+# Example : MY_PROGRAM = 'ELAVMS0000000467'
+MY_PROGRAM = "ELAVMS0000000198"
 
 
 # Create a dataframe to store the configs
-configs = pd.DataFrame(configs_list(api_access_token, MY_PROGRAM)['configs'])
+configs = pd.DataFrame(configs_list(api_access_token, MY_PROGRAM)["configs"])
 
 # Lists of selected dataframe columns to keep from the dataframes
-conf_cols = ['id', 'name', 'productType', 'programSerialNumber','status']
-vms_cols = ['serialNumber', 'description', 'endDate','token', 'tokenStatus' ]
+conf_cols = ["id", "name", "productType", "programSerialNumber", "status"]
+vms_cols = ["serialNumber", "description", "endDate", "token", "tokenStatus"]
 points_cols = []
 
 # Using the current month start and end dates
 # Feel free to change them to your needs
-start_date = get_first_date_of_current_month(datetime.now().year, datetime.now().month)
-end_date = get_last_date_of_month(datetime.now().year, datetime.now().month)
+r_start_date = get_first_date_of_current_month(datetime.now().year, datetime.now().month)
+r_end_date = get_last_date_of_month(datetime.now().year, datetime.now().month)
 
-print(start_date)
-print(end_date)
+print(r_start_date)
+print(r_end_date)
 
-# For each CONFIG available and for the date range selected, print the config name, ID & parameters, the VM list associated and the points consumed by VM.
+# For each CONFIG available and for the date range selected,
+# print the config name, ID & parameters, the VM list associated
+# and the points consumed by VM.
+
 for idx in range(configs.shape[0]):
-    print('#' * 120 + '\n' + '#' * 120 + '\n')
-    print('\nConfig\n------\n')
-    print(configs.iloc[idx][conf_cols].to_markdown(tablefmt='grid'))
-    print('\nConfig Parameters\n-----------------\n')
-    print(pd.DataFrame(configs.iloc[idx]['parameters']).to_markdown(tablefmt='grid', index = False))
-    print('\nVM list\n-------\n')
-    prog = int(configs.iloc[idx]['id'])
-    vms= pd.DataFrame(vms_list(api_access_token, prog)['vms'])
-    if(vms.shape[0] != 0):
-        print(vms[vms_cols].to_markdown(tablefmt='grid', index = False))
+    print("#" * 120 + "\n" + "#" * 120 + "\n")
+    print("\nConfig\n------\n")
+    print(configs.iloc[idx][conf_cols].to_markdown(tablefmt="grid"))
+    print("\nConfig Parameters\n-----------------\n")
+    print(
+        pd.DataFrame(configs.iloc[idx]["parameters"]).to_markdown(
+            tablefmt="grid", index=False
+        )
+    )
+    print("\nVM list\n-------\n")
+    prog = int(configs.iloc[idx]["id"])
+    vms = pd.DataFrame(vms_list(api_access_token, prog)["vms"])
+    if vms.shape[0] != 0:
+        print(vms[vms_cols].to_markdown(tablefmt="grid", index=False))
     else:
         print("No VMs available for this config")
-    print('\n')
-    print('\nPoints by Config\n--------------\n')
-    print('Start Date: ' + start_date)
-    print('End Date: ' + end_date)
-    points = vms_points_by_config_id(api_access_token, prog, start_date, end_date)
-    if(points['vms'] != []):
-        print(pd.DataFrame(points['vms'])[['serialNumber', 'points']].to_markdown(tablefmt='grid', index = False))
+    print("\n")
+    print("\nPoints by Config\n--------------\n")
+    print("Start Date: " + r_start_date)
+    print("End Date: " + r_end_date)
+    points = vms_points_by_config_id(api_access_token, prog, r_start_date, r_end_date)
+    if points["vms"] != []:
+        print(
+            pd.DataFrame(points["vms"])[["serialNumber", "points"]].to_markdown(
+                tablefmt="grid", index=False
+            )
+        )
     else:
-        print('No consumption identified')
-    print('\n')
-    
-
-
-
+        print("No consumption identified")
+    print("\n")
